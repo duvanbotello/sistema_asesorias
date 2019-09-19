2cd71554c8dc619b2454d669eadb4676baf4cb8d
dbotello <dbotello@ab-sistemas.com>
2019-09-19 15:30:11 -0500

    Creando estructura del proyecto

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    //Esta es la manera de declarar constatntes
+    const LBS = 'Library/';
+    const VIEWS = 'Views/';
+    //otra forma de declarar constantes utilizando la funcion define revise por parametro el nombre
+    //de la variable y el valor de la constante
+    define('DFT', 'Default/');
+    define('PCP', 'Principal/');
+    define('RQ','Resource/');
+    define('URL','http://localhost/profesional/');
+?>
